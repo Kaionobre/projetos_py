c39770187e1e9d459535b96fbb2b775727a1ee31
Kaionobre <kaionobrega00@gmail.com>
2023-01-27 20:51:40 -0300

    Main.py Não sei oq to fazendo

---
+++ aula_08.py
@@ -0,0 +1,22 @@
+nome = 'Kaio'
+sobrenome = 'Brito'
+idade = 18
+ano_nascimento = 2004
+maior_de_idade = idade >=18
+altura_metros = 1.80
+
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano do nascimento:', ano_nascimento)
+print('É maior de idade?:', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
+
+
+
+
+
+
---
+++ aula_09.py
@@ -0,0 +1,9 @@
+nome = 'Kaio'
+peso = 60
+altura = 1.77
+imc = peso / altura ** 2
+
+print (f'O seu nome é {nome} ')
+print (f'O seu peso é {peso}')
+print (f'Sua altura é {altura}')
+print (f'O seu imc da {imc}')
