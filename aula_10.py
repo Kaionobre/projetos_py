358df9cce2e21e86a7fbb3cd78db4b0249cc09b9
Kaionobre <kaionobrega00@gmail.com>
2023-01-28 10:23:01 -0300

    novo exercício aula 10 novo exercício aula 10.

---
+++ aula_10.py
@@ -0,0 +1,16 @@
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+"""
+
+nome = 'kaio'
+preco = 2000.84786532
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
+print('O exadecimal de %d é %08X' % (1500, 1500))
+
+
+
