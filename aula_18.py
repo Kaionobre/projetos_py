54186bfcbf78eb73a699b178d75b1bd3aea68e47
Kaionobre <kaionobrega00@gmail.com>
2023-02-08 12:59:12 -0300

    Main

---
+++ aula_18.py
@@ -0,0 +1,22 @@
+# https://docs.python.org/pt-br/3/library/stdtypes.html
+
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'kaio Nóbrega'
+
+print (string.upper())
+print (string.lower())
+
+string_01 = '1000'
+
+
+
+
+
+
+
+# outra_variavel = f'{string[:3]}ABC{string[4:]}'
+# print(string)
+# print(outra_variavel)
