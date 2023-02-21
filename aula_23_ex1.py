97efd193b65618b0e15456b8299eb163dcc5109e
Kaionobre <kaionobrega00@gmail.com>
2023-02-21 13:17:33 -0300

    Main

--- aula_23_ex1.py
+++ aula_23_ex1.py
@@ -2,5 +2,40 @@
 Iterando strings com while
 """
 
+"""
+Basicamente eu teria que criar uma estrutura de repetição usando o whiler e em cara iteração na string do nome teria que se colocar um caractere especial. EX: #$%&*. Abaixo foi a minha tentativa, infelizmente não saí do looping infinito. 
+"""
+
+"""
 nome = 'Kaio Nóbrega'
+
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+print(nome[3])
+
+i = nome
+while len(nome) <= 12:
+    print(i)
+    i += '#'
+"""
+
+
+
+    
+"""
+Abaixo o que o professor fez
+"""
+
+nome = 'Kaio Nóbrega'
+
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome [indice]
+    novo_nome += letra
+    indice += 1
+    
+print(novo_nome)
+
 
