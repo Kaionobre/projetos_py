5802e4a41674bcca2aa75db96def8bad0b64b137
Kaionobre <kaionobrega00@gmail.com>
2023-02-28 17:48:11 -0300

    Main

--- aula_24_ex1.py
+++ aula_24_ex1.py
@@ -2,10 +2,44 @@
 
 #startswith - começa com
 #endswith   - termina com
-
+"""
 while True:
     sair = input('Quer sair? [s]im: ').lower().startswith('s' or 'S')
     print(sair)
 
     if sair is True:
-        ...
+        break
+"""
+
+while True:
+    numero_1 = input('Digite um número') 
+    numero_2 = input('Digite outro número') 
+    operador = input('Digite o operador (+-/*)') 
+    
+    numeros_validos = None
+    
+    try:
+        num_1 = float(numero_1)
+        num_2 = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+        
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados não são válidos')
+        continue
+    
+    operadores_permitidos = '+-*/'
+    
+    if operador not in operadores_permitidos:
+        print('Operador inválidos')
+        continue
+        
+    if len(operador) > 1:
+        print('Digite apenas um operador')
+        continue  
+    
+    sair = input('Quer sair? [S]im: ').lower().startswith('s')
+    
+    if sair is True:
+        break
