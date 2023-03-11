a3f7be6647e7cc67cb6f4d60f33a15eb86366d83
Kaionobre <kaionobrega00@gmail.com>
2023-03-11 11:53:34 -0300

    Main

--- aula_24_ex1.py
+++ aula_24_ex1.py
@@ -12,11 +12,13 @@ while True:
 """
 
 while True:
-    numero_1 = input('Digite um número') 
-    numero_2 = input('Digite outro número') 
-    operador = input('Digite o operador (+-/*)') 
+    numero_1 = input('Digite um número: ') 
+    numero_2 = input('Digite outro número: ') 
+    operador = input('Digite o operador (+-/*): ') 
     
     numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
     
     try:
         num_1 = float(numero_1)
@@ -38,6 +40,19 @@ while True:
     if len(operador) > 1:
         print('Digite apenas um operador')
         continue  
+    
+    print('Realizando sua conta. Confira o resultado abaixo:')
+    
+    if operador == '+':
+        ...
+    elif operador == '-':
+        ...
+    elif operador == '/':
+        ...
+    elif operador == '*':
+        ...
+    else:
+        print('Nunca deveria chegar aqui')
     
     sair = input('Quer sair? [S]im: ').lower().startswith('s')
     
---
+++ teste.py
@@ -0,0 +1,5 @@
+cod, qnt, preco = input().split()
+
+preco_float = float(preco)
+
+print(cod, qnt, preco) 
