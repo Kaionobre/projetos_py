ce7e68ba37008f77458549cce413f4c464c2a984
Kaionobre <kaionobrega00@gmail.com>
2023-04-25 17:54:30 -0300

    Main

--- aula_00.py
+++ aula_00.py
@@ -1,5 +1,50 @@
-hello_word = 0
+#beecrowd
+"""
+#1059
 
-while hello_word < 101:
-    hello_word = hello_word + 1
-    print('Hello World')
+numero = 0
+
+while numero < 100:  
+    numero = numero + 2
+    print(numero)
+"""
+    
+"""
+#1064
+
+contador = 0
+positivo = 0
+media = 0
+
+while contador < 6:
+    entrada = float(input())
+    if entrada >= 0:
+        media = entrada + media
+        positivo += 1
+    contador += 1
+
+media_final = media / positivo
+
+print(f'{positivo} valores positivos')
+print(f'{media_final}') 
+
+"""
+
+#1067
+
+x = int(input())
+numero = 1
+
+while True:
+    if numero >= x:
+        break
+
+
+    
+
+
+
+
+
+
+
