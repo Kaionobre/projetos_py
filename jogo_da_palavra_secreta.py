7439fd43288983f943f2236054c28bb5d39444e0
Kaionobre <kaionobrega00@gmail.com>
2023-05-29 17:38:04 -0300

    Introdução às listas em Python

--- jogo_da_palavra_secreta.py
+++ jogo_da_palavra_secreta.py
@@ -31,33 +31,36 @@ while contador < len(palavra_secreta):
     
 """
 
+import os
+
 palavra_secreta = 'perfume'
 letras_acertadas = ''
-contador = 0
+numero_tentativas = 0
 
 while True:
-    contador += 1
-    print(f'Tentativa N° {contador}')
     letra_digitada = input('Digite uma letra: ')
-    
+    numero_tentativas += 1
+
     if len(letra_digitada) > 1:
         print('Digite apenas uma letra.')
         continue
-    
+
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
-        
+
     palavra_formada = ''
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
-            palavra_formada += letra_secreta 
+            palavra_formada += letra_secreta
         else:
             palavra_formada += '*'
-            
-    print(f'Palavra formada: {palavra_formada}')
- 
+
+    print('Palavra formada:', palavra_formada)
+
     if palavra_formada == palavra_secreta:
-        print('EBAA Temos um Sherlok Homes aqui!!') 
-        print(f'A palavra era {palavra_secreta}')  
-    
-    
+        os.system('clear')
+        print('VOCÊ GANHOU!! PARABÉNS!')
+        print('A palavra era', palavra_secreta)
+        print('Tentativas:', numero_tentativas)
+        letras_acertadas = ''
+        numero_tentativas = 0
